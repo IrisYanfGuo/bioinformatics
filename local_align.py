ac2b3636d7a164ffe2e3c8f790ff6de678093581
IrisGuo <yanfguo@vub.ac.be>
2017-03-11 01:53:52 +0100

    before change seq1 and seq2

--- local_align.py
+++ local_align.py
@@ -1,27 +1,28 @@
+# in a non recursive way
 from Matrix import *
 from read_fasta import *
 
+# affine gap penalty
+affine = -0
+gap = -8
+
 seq_list = read_fasta("protein-sequences.fasta")
-seq1 = seq_list[0][:100]
-seq2 = seq_list[1][:100]
+seq1 ='THISLINE'
+seq2 = 'ISALIGNED'
 slen1 = len(seq1)
 slen2 = len(seq2)
-mat1 = Matrix("BLOSUM50.txt")
-print(seq1, seq2)
+mat1 = Matrix("BLOSUM62.txt")
+print(seq1)
+print(seq2)
 
 score_mat = []
+direc_mat = []
+for i in range(len(seq1) + 1):
+    direc_mat.append([[0, 0, 0,0] for i in range(len(seq2) + 1)])
 
 for i in range(len(seq1) + 1):
     score_mat.append([0 for i in range(len(seq2) + 1)])
-
-# create the direction mat
-direc_mat = []
-for i in range(len(seq1) + 1):
-    direc_mat.append([[0, 0, 0] for i in range(len(seq2) + 1)])
-# [0,1,0] means seq 2 has a gap, horizontal way
-# [1,0,0] means seq1 has a gap, vertical way
-# [0,0,1] means diagonal
-print(direc_mat)
+# initialize
 
 
 def print_mat2(alist):
@@ -36,7 +37,7 @@ def print_mat3(alist):
         print(i)
 
 
-def local_score(mat, gap):
+def local_score(mat):
     for i in range(1, len(seq1) + 1):
         for j in range(1, len(seq2) + 1):
             t1 = score_mat[i - 1][j - 1] + mat.get(seq1[i - 1], seq2[j - 1])
@@ -44,17 +45,18 @@ def local_score(mat, gap):
             # [s(i,j-n(gap)+g(gap]
             t2 = []
             for k in range(j - 1, -1, -1):
-                t = score_mat[i][k] + gap * (j - k)
-                if (t < gap):
+                t = score_mat[i][k] + gap * (j - k) + int((j-k)*(j-k-1)*affine/2)
+                if (t < 0):
                     break
                 t2.append(t)
-            t3 = []
 
+            t3 = []
             for k in range(i - 1, -1, -1):
-                t = score_mat[i][k] + gap * (j - k)
-                if (t < gap):
+
+                t = score_mat[k][j] + gap * (i - k) + int((j-k-1)*(j-k)*affine/2)
+                if (t < 0):
                     break
-                t3.append(score_mat[k][j] + gap * (i - k))
+                t3.append(t)
 
             if len(t2) > 0:
                 t2max = max(t2)
@@ -71,6 +73,7 @@ def local_score(mat, gap):
             if max_score > 0:
                 if max_score == t1:
                     direc_mat[i][j][2] = 1
+
                 elif max_score == t2max:
                     # horizental
                     direc_mat[i][j][1]=1
@@ -78,8 +81,12 @@ def local_score(mat, gap):
                     # vertical, seq1 has a gap
                     direc_mat[i][j][0]=1
 
+a,i = max([1,2,3]),argmax([1,2,3])
 
 
 
-local_score(mat1, -10)
+
+local_score(mat1)
 print_mat2(score_mat)
+print_mat3(direc_mat)
+
