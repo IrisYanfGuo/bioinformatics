0081a05f5344ad8c6371a099b50aa37b0a43e669
IrisGuo <yanfguo@vub.ac.be>
2017-04-11 22:15:20 +0200

    GOR3

---
+++ sec_str/gor2.py
@@ -0,0 +1,161 @@
+from pre_process import *
+from numpy import log
+import matplotlib.pyplot as plt
+
+dssp = pre_process("dssp_info.txt", "dssp.txt")
+stride = pre_process("stride_info.txt", "stride.txt")
+
+fsr,fs,fr= fSR(dssp)
+#print(fs)
+#print(fr)
+total = fs['C']+fs['E']+fs['C']
+
+# begin gor3 algorithm
+
+
+def gor2(dssp,resultfile):
+    predict2 = []
+    for i in range(len(dssp)):
+
+        helix = log(fsr['H'][dssp[i][3]] / (fsr['C'][dssp[i][3]] + fsr['E'][dssp[i][3]])) + log(
+            (fs['E'] + fs['C']) / fs['H'])
+        for j in range(-8, 9):
+            if j != 0:
+                if i + j > 0 and i + j < len(dssp) - 1:
+                    # here now use the gor ii
+                    t = i + j
+                    if dssp[t][0] == dssp[i][0] and dssp[t][1] == dssp[i][1]:
+                        helix += log(fsr['H'][dssp[t][3]] / (fsr['C'][dssp[t][3]] + fsr['E'][dssp[t][3]])) + log(
+                            (fs['E'] + fs['C']) / fs['H'])
+
+        coil = log(fsr['C'][dssp[i][3]] / (fsr['H'][dssp[i][3]] + fsr['E'][dssp[i][3]])) + log(
+            (fs['E'] + fs['H']) / fs['C'])
+        for j in range(-8, 9):
+            if j != 0:
+                if i + j > 0 and i + j < len(dssp) - 1:
+                    # here now use the gor ii
+                    t = i + j
+                    if dssp[t][0] == dssp[i][0] and dssp[t][1] == dssp[i][1]:
+                        coil = log(fsr['C'][dssp[t][3]] / (fsr['H'][dssp[t][3]] + fsr['E'][dssp[t][3]])) + log(
+                            (fs['E'] + fs['H']) / fs['C'])
+
+        sheet = log(fsr['E'][dssp[i][3]] / (fsr['H'][dssp[i][3]] + fsr['C'][dssp[i][3]])) + log(
+            (fs['C'] + fs['H']) / fs['E'])
+        for j in range(-8, 9):
+            if j != 0:
+                if i + j > 0 and i + j < len(dssp) - 1:
+                    # here now use the gor ii
+                    t = i + j
+                    if dssp[t][0] == dssp[i][0] and dssp[t][1] == dssp[i][1]:
+                        coil = log(fsr['E'][dssp[t][3]] / (fsr['H'][dssp[t][3]] + fsr['C'][dssp[t][3]])) + log(
+                            (fs['C'] + fs['H']) / fs['E'])
+
+        if coil == max(sheet, coil, helix):
+            predict2.append('C')
+        elif sheet == max(sheet, coil, helix):
+            predict2.append('E')
+
+        else:
+
+            predict2.append('H')
+
+    f = open(resultfile, 'w')
+    f.write("".join(predict2))
+    f.write("\n")
+
+    right = ''.join([dssp[i][4] for i in range(len(dssp))])
+    print(''.join(predict2))
+    f.write(right)
+    f.close()
+    print(right)
+
+
+
+
+def gor2_list(alist):
+    predict2 = []
+    for i in range(len(alist)):
+
+        helix = log(fsr['H'][alist[i]] / (fsr['C'][alist[i]] + fsr['E'][alist[i]])) + log(
+            (fs['E'] + fs['C']) / fs['H'])
+        for j in range(-8, 9):
+            if j != 0:
+                if i + j > 0 and i + j < len(alist) - 1:
+                    # here now use the gor ii
+                    t = i + j
+
+                    helix += log(fsr['H'][alist[t]] / (fsr['C'][alist[t]] + fsr['E'][alist[t]])) + log(
+                        (fs['E'] + fs['C']) / fs['H'])
+
+        coil = log(fsr['C'][alist[i]] / (fsr['H'][alist[i]] + fsr['E'][alist[i]])) + log(
+            (fs['E'] + fs['H']) / fs['C'])
+        for j in range(-8, 9):
+            if j != 0:
+                if i + j > 0 and i + j < len(alist) - 1:
+                    # here now use the gor ii
+                    t = i + j
+
+                    coil = log(fsr['C'][alist[t]] / (fsr['H'][alist[t]] + fsr['E'][alist[t]])) + log(
+                        (fs['E'] + fs['H']) / fs['C'])
+
+        sheet = log(fsr['E'][alist[i]] / (fsr['H'][alist[i]] + fsr['C'][alist[i]])) + log(
+            (fs['C'] + fs['H']) / fs['E'])
+        for j in range(-8, 9):
+            if j != 0:
+                if i + j > 0 and i + j < len(alist) - 1:
+                    # here now use the gor ii
+                    t = i + j
+
+                    coil = log(fsr['E'][alist[t]] / (fsr['H'][alist[t]] + fsr['C'][alist[t]])) + log(
+                        (fs['C'] + fs['H']) / fs['E'])
+
+        if coil == max(sheet, coil, helix):
+            predict2.append('C')
+        elif sheet == max(sheet, coil, helix):
+            predict2.append('E')
+
+        else:
+
+            predict2.append('H')
+    return ''.join(predict2)
+
+alistt = 'ADPPPVHDTDGHELRADANYYVLSANRAHGGGLTMAPGHGRHCPLFVSQDPNGQHDGFPVRITPYGVAPSDKIIRLSTDVRISFRAYTTCLQSTEWHIDSELAAGRRHVITGPVKDPSPSGRENAFRIEKYSGAEVHEYKLMSCGDWCQDLGVFRDLKGGAWFLGATEPYHVVVFKKAPPA'
+print(gor2_list(alistt))
+a='CCCCECECCCCCECECCCEEEEEECCHHHCCCEEEEEECCEEEEEEEEECCCCCCCCCCEEEEECCCCCCCCECECCCCEEEEECCCCCCCCCCECEECCCCECCECEEECCCCCCCCCCCHHHCEEEEECECCCCCCEEEEEECCCEEECEEECCCCCCCCEEECCCCECCEEEEEECCC'
+#gor2(dssp,'predict2.txt')
+
+
+def printresult(str,str2):
+    plt.figure(figsize=(len(str),2))
+    for i in range(len(str)):
+        if str[i]=='C':
+            plt.plot(i, 1, label='o', marker="+", markersize=30, color='r')
+        elif str[i]=='E':
+            plt.plot(i, 1, label='o', marker="+", markersize=30, color='g')
+        else:
+            plt.plot(i, 1, label='o', marker="+", markersize=30, color='b')
+    for i in range(len(str2)):
+        if str2[i]=='C':
+            plt.plot(i, 2, label='o', marker="+", markersize=30, color='r')
+        elif str2[i]=='E':
+            plt.plot(i, 2, label='o', marker="+", markersize=30, color='g')
+        else:
+            plt.plot(i, 2, label='o', marker="+", markersize=30, color='b')
+    plt.show()
+
+#printresult(gor2_list(alistt),a)
+
+def printwrong(str,str2):
+    plt.figure(figsize=(len(str2),2))
+    rate=0
+    for i in range(len(str)):
+        if str[i]==str2[i]:
+            plt.plot(i, 2, label='o', marker="+", markersize=30, color='b')
+            rate +=1
+        else:
+
+            plt.plot(i, 2, label='o', marker="+", markersize=30, color='r')
+    print("right",rate/len(str) )
+    plt.show()
+
+printwrong(gor2_list(alistt),a)
--- sec_str/gor3.py
+++ sec_str/gor3.py
@@ -5,41 +5,38 @@ import matplotlib.pyplot as plt
 dssp2 = pre_process("dssp_info.txt", "dssp.txt")
 stride = pre_process("stride_info.txt", "stride.txt")
 
-fsr,fs,fr= fSR(dssp2)
-#print(fs)
-#print(fr)
-total = fs['C']+fs['E']+fs['C']
-
-
+fsr, fs, fr = fSR(dssp2)
+# print(fs)
+# print(fr)
+total = fs['C'] + fs['E'] + fs['C']
 
 # do some pre_process, read the protein sequence and structure in to a 2d list
 f = open("dssp_protein")
 
 list = f.readlines()
-list2 =[]
-for i in range(0,len(list),3):
-    temp =[]
-    temp.append(list[i+1].strip())
-    temp.append(list[i+2].strip())
+list2 = []
+for i in range(0, len(list), 3):
+    temp = []
+    temp.append(list[i + 1].strip())
+    temp.append(list[i + 2].strip())
     list2.append(temp)
 f.close()
 # dealing the statistics
-fsrm={}
+fsrm = {}
 # initialize
-amino =[]
+amino = []
 for i in amino_dict.keys():
     amino.append(amino_dict[i])
 amino.append('?')
 print(amino)
-for i in ['C','E','H']:
+for i in ['C', 'E', 'H']:
     temp3 = {}
     for j in amino:
         temp2 = {}
         for k in amino:
             temp = {}
-            for n in range(1,9):
-
-                temp[n]=0
+            for n in range(1, 9):
+                temp[n] = 0
 
             temp2[k] = temp
 
@@ -48,8 +45,49 @@ for i in ['C','E','H']:
 
 for i in list2:
     for j in range(len(i[0])):
-        for k in range(1,9):
-            fsrm[i[1][j]][i[0][j]][i[0][k]][k] +=1
+        for k in range(1, 9):
+            fsrm[i[1][j]][i[0][j]][i[0][k]][k] += 1
 
 for i in fsrm.keys():
     print(fsrm[i])
+
+
+def gor3(alist):
+    result = []
+    for i in range(len(alist)):
+
+        helix = log(fsr['H'][alist[i]] / (fsr['C'][alist[i]] + fsr['E'][alist[i]])) + log(
+            (fs['E'] + fs['C']) / fs['H'])
+        coil = log(fsr['C'][alist[i]] / (fsr['H'][alist[i]] + fsr['E'][alist[i]])) + log(
+            (fs['H'] + fs['E']) / fs['C'])
+        sheet = log(fsr['E'][alist[i]] / (fsr['C'][alist[i]] + fsr['H'][alist[i]])) + log(
+            (fs['H'] + fs['C']) / fs['E'])
+        for j in range(-8, 9):
+            t = i + j
+            if t > 0 and t < len(alist):
+                if j != 0:
+                    helix += log(fsrm['H'][alist[i]][alist[t]][abs(j)] / (
+                    fsrm['E'][alist[i]][alist[t]][abs(j)] + fsrm['C'][alist[i]][alist[t]][abs(j)])) + log(
+                        (fsr['E'][alist[i]] + fsr['C'][alist[i]]) / fsr['H'][alist[i]])
+
+                    coil += log(fsrm['C'][alist[i]][alist[t]][abs(j)] / (
+                        fsrm['E'][alist[i]][alist[t]][abs(j)] + fsrm['H'][alist[i]][alist[t]][abs(j)])) + log(
+                        (fsr['E'][alist[i]] + fsr['H'][alist[i]]) / fsr['C'][alist[i]])
+                    sheet += log(fsrm['E'][alist[i]][alist[t]][abs(j)] / (
+                        fsrm['H'][alist[i]][alist[t]][abs(j)] + fsrm['C'][alist[i]][alist[t]][abs(j)])) + log(
+                        (fsr['H'][alist[i]] + fsr['C'][alist[i]]) / fsr['E'][alist[i]])
+
+
+        if coil == max(sheet, coil, helix):
+            result.append('C')
+        elif sheet == max(sheet, coil, helix):
+            result.append('E')
+
+        else:
+            result.append('H')
+    return ''.join(result)
+
+alistt = 'ADPPPVHDTDGHELRADANYYVLSANRAHGGGLTMAPGHGRHCPLFVSQDPNGQHDGFPVRITPYGVAPSDKIIRLSTDVRISFRAYTTCLQSTEWHIDSELAAGRRHVITGPVKDPSPSGRENAFRIEKYSGAEVHEYKLMSCGDWCQDLGVFRDLKGGAWFLGATEPYHVVVFKKAPPA'
+print(gor3(alistt))
+a='CCCCECECCCCCECECCCEEEEEECCHHHCCCEEEEEECCEEEEEEEEECCCCCCCCCCEEEEECCCCCCCCECECCCCEEEEECCCCCCCCCCECEECCCCECCECEEECCCCCCCCCCCHHHCEEEEECECCCCCCEEEEEECCCEEECEEECCCCCCCCEEECCCCECCEEEEEECCC'
+print(a)
